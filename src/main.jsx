d76835ff4cf786009e7adc08fdd246233bcafdd4
Srajan Bansal <srajanbansal999@gmail.com>
2024-02-12 00:52:53 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import StarRating from './StarRating';
+// import App from './App.jsx';
+// import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		{/* <App /> */}
+		<StarRating maxRating={5} />
+		{/* <StarRating maxRating={10} />
+		<StarRating max /> */}
+	</React.StrictMode>
+);
